0710ff1454d20fe0e993d79ae8f51655b1f0863d
GB-LD <gbld@mailo.com>
2022-12-15 10:17:49 +0100

    denied access to profil page without user & change path of homepage

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController
 {
     #[Route('/profil', name: 'app_dashboard')]
-    public function show(): Response
+    public function show(Security $security): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER', null, "Pour accéder à cette page, veuillez vous connecter");
         return $this->render('dashboard/dashboard.html.twig');
     }
 }
